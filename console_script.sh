a35ab754b1705917b8fc3a1f3bd62cee2c4a7251
Brandon Hu <hubrando@cse.msu.edu>
2020-11-16 20:26:22 -0500

    Add GCE specific commands.

--- console_script.sh
+++ console_script.sh
@@ -7,13 +7,22 @@ echo "==========================================================================
 echo " :gcloud info" >> $OUTPUT
 gcloud info >> $OUTPUT
 echo "======================================================================================" >> $OUTPUT
-echo "Project networking configurations" >> $OUTPUT
+echo "Project Networking Configurations" >> $OUTPUT
 echo "======================================================================================" >> $OUTPUT
 echo " :gcloud compute networks list" >> $OUTPUT
 gcloud compute networks list >> $OUTPUT
-echo "=====================================================================================" >> $OUTPUT
+echo "======================================================================================" >> $OUTPUT
+echo "Google Compute Engine Specific Information"
+echo "======================================================================================" >> $OUTPUT
+echo " :gcloud compute instances list"
+gcloud compute instances list >> $OUTPUT
+echo " :gcloud compute images list"
+gcloud compute images list >> $OUTPUT
+echo " :gcloud compute project-info describe"
+gcloud compute project-info describe >> $OUTPUT
+echo "======================================================================================" >> $OUTPUT
 echo "IAM Role information" >> $OUTPUT
-echo "=====================================================================================" >> $OUTPUT
+echo "======================================================================================" >> $OUTPUT
 echo " :gcloud projects get-iam-policy $PROJECT_ID" >> $OUTPUT
 gcloud projects get-iam-policy $PROJECT_ID >> $OUTPUT
 
